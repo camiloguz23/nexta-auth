4d4bdeb960308a0f8ea7498d905f0fa668f4085f
jeyson <jeysonkmguzman@gmail.com>
2024-01-07 17:32:30 -0500

    feat: CI/CD 6 with auth

--- src/components/ui-btn-login/ui-btn-login.tsx
+++ src/components/ui-btn-login/ui-btn-login.tsx
@@ -1,19 +1,7 @@
 "use client";
 
-import { signIn, useSession, signOut } from "next-auth/react";
-
 function UiBtnLogin() {
-  const { data: session } = useSession();
-  console.log(session);
-  return (
-    <button
-      onClick={() =>
-        session ? signOut() : signIn("google", { callbackUrl: "/" })
-      }
-    >
-      {session ? "LogOut" : "Login"}
-    </button>
-  );
+  return <button>login</button>;
 }
 
 export default UiBtnLogin;
