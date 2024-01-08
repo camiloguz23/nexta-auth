2506d1259c3f2eaebbd29699a3aef5656c22e149
jeyson <jeysonkmguzman@gmail.com>
2024-01-08 09:04:24 -0500

    feat: CI/CD node 35

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "loginjs",
-        script: "./",
+        script: "./node_modules/next/dist/bin/next",
         args: "start",
         watch: false,
         autorestart: true,
